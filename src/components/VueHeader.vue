06ab48c25db60c0a7fbb12e95c8384c8f5fa680c
Michele Santoro <michelesantoro2000@icloud.com>
2023-04-11 13:48:40 +0200

    Inizio Progetto

---
+++ src/components/VueHeader.vue
@@ -0,0 +1,39 @@
+<script>
+    export default {
+        name: "VueHeader",
+        data() {
+            return {
+
+            }
+        }
+
+    }
+</script>
+
+<template>
+
+    <header>
+        <div>
+
+            <div class="topHead">
+
+            </div>
+
+            <div class="mainHead">
+
+            </div>
+
+            <div class="endHead">
+
+            </div>
+
+        </div>
+
+
+    </header>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
